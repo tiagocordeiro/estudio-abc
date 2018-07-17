3169db0a2d230958dc84532570e9d17c9df9830f
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-17 03:35:51 -0300

    Configura Cloudinary

--- website/settings.py
+++ website/settings.py
@@ -136,11 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
-# Configuration for dev environment
-MEDIA_URL = f'/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 COLLECTFAST_ENABLED = False
 
 
