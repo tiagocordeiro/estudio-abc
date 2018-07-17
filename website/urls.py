3115a4edf166dd40b0d62b3c564a6577efcbba8e
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-17 07:36:22 -0300

    Fix favicon Heroku error

--- website/urls.py
+++ website/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
@@ -28,5 +28,4 @@ admin.site.site_header = 'Estúdio ABC'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    url(r'^', include('favicon.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
