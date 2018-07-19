008293148facee5abc6da34a54803f6dd5e5a086
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-19 13:10:52 -0300

    Prepara testes

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,21 @@
-# from django.test import TestCase
+import pytest
+from django.conf import settings
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+from website.django_assertions import dj_assert_contains, \
+    dj_assert_not_contains, dj_assert_template_used
+
+
+@pytest.fixture
+def home_resp(client):
+    return client.get('/', secure=True)
+
+
+def test_home_status_code(home_resp):
+    assert 200 == home_resp.status_code
+
+
+def test_home_template(home_resp):
+    dj_assert_template_used(home_resp, template_name='pages/index.html')
