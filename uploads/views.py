36633c2e2c6cc02c774c270c404d46f2e48af0b4
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-20 01:54:54 -0300

    Adiciona login_required em uploads

--- uploads/views.py
+++ uploads/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Document
 from .forms import DocumentForm
 
 
+@login_required
 def arquivos(request):
     documents = Document.objects.all()
     return render(request, 'uploads/list_files.html', {'documents': documents})
 
 
+@login_required
 def model_form_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
