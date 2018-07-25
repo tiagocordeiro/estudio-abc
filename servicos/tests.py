86329feee2719e5022f491d43cde27677d668fec
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-25 15:06:57 -0300

    Configura testes para Serviços

--- servicos/tests.py
+++ servicos/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+import pytest
+# from django.conf import settings
+# from django.test import Client
+# from django.urls import reverse
 
-# Create your tests here.
+
+# from website.django_assertions import dj_assert_template_used
+
+
+@pytest.fixture
+def servicos_home_resp(client):
+    return client.get('/dashboard/servicos/', secure=True)
+
+
+def test_servicos_home_status_code(servicos_home_resp):
+    assert 302 == servicos_home_resp.status_code
